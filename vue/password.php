289b19ad97e00224fa7fd3e6e43b6f9d1d122139
as199 <57167524+as199@users.noreply.github.com>
2020-06-13 14:33:56 +0000

    Update password.php

--- vue/password.php
+++ vue/password.php
@@ -32,14 +32,14 @@
                                     <!-- <h3 class="text-center font-weight-light my-4">Password Recovery</h3> -->
                                 </div>
                                 <div class="card-body">
-                                    <div class="small mb-3 text-muted">Enter your email address and we will send you a link to reset your password.</div>
-                                    <form>
-                                        <div class="form-group"><label class="small mb-1" for="inputEmailAddress">Email</label><input class="form-control py-4" id="inputEmailAddress" type="email" aria-describedby="emailHelp" placeholder="Enter email address" /></div>
-                                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"><a class="small" href="login.php">Return to login</a><a class="btn btn-primary" href="login.php">Reset Password</a></div>
+                                    <div class="small mb-3 text-muted" id="sms">Enter your email address and we will send you a link to reset your password.</div>
+                                    <form method="post">
+                                        <div class="form-group"><label class="small mb-1" for="email">Email</label><input class="form-control py-4" id="email" type="email" aria-describedby="emailHelp" placeholder="Enter email address" /></div>
+                                        <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"><a class="small" href="../index.php">Return to login</a><input type="submit" class="btn btn-primary" id="reset" value="Reset Password" /></div>
                                     </form>
                                 </div>
-                                <div class="card-footer text-center">
-                                    <div class="small"><a href="register.php">Need an account? Sign up!</a></div>
+                                <div class=" card-footer text-center">
+                                    <div class="small"><a href="./register.php">Need an account? Sign up!</a></div>
                                 </div>
                             </div>
                         </div>
@@ -64,7 +64,38 @@
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script>
+        $(document).ready(function() {
+            $("#reset").click(function(e) {
+                e.preventDefault();
+                var email = $("#email").val();
+                $.ajax({
+                    url: "../traitement/action.php",
+                    type: "POST",
+                    data: {
+                        email: email
+                    },
+                    success: function(response) {
+                        data = JSON.parse(response);
+                        if (data.error != 1) {
+                            $('#sms').html('Votre Password est :' + data.error);
+                            $('#sms').css('font-weight', 'bold');
+                            $('#sms').css('font-size', '25px');
+                            $('#email').val('');
+                        } else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'this email is not linked to any user!',
+                                showConfirmButton: false,
+                                timer: 2000
+                            })
+                        }
+                    }
+                });
+            });
+        });
+    </script>
 </body>
 
 </html>
