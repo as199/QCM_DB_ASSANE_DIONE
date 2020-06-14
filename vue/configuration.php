d2e05c944cd4b215c3efce0397f463c300af760d
as199 <57167524+as199@users.noreply.github.com>
2020-06-14 12:59:09 +0000

    Update configuration.php

--- vue/configuration.php
+++ vue/configuration.php
@@ -16,7 +16,7 @@ if(empty($_SESSION['connecter'])){
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Page Title - SB Admin</title>
+    <title>SamaJeux</title>
     <link href="../ressources/css/styles.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
